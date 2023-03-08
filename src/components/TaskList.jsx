c4d2ab0bddc4f0b29440c53c82da66030e7de09a
Jocsan Morera <joey_morera13@hotmail.com>
2023-03-08 15:08:43 -0600

    styles update

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -11,7 +11,7 @@ function TaskList() {
         return <h1 className="text-white text-4xl font-bold text-center">No Tasks Yet</h1>
     }
     return (
-        <div className="grid grid-cols-4 gap-2">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2">
             {task.map((tarea, index) => (
                     <TaskCard key={index} tarea={tarea} />
                 ))}
