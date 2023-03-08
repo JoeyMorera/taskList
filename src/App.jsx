cc5656987bdd769bb495cd1d8c749cecc76c68d8
Jocsan Morera <joey_morera13@hotmail.com>
2023-03-08 15:30:51 -0600

    update responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TaskList from './components/TaskList'
 
 function App() {
   return (
-    <main className='bg-zinc-900 min-h-full'>
+    <main className='bg-zinc-900 h-full'>
       <div className='container mx-auto p-10'>
         <TaskForm />
         <TaskList />
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -8,10 +8,10 @@ function TaskList() {
     const {task} = useContext(TaskContext)
     
     if (task.length === 0) {
-        return <h1 className="text-white text-4xl font-bold text-center">No Tasks Yet</h1>
+        return <h1 className="text-white text-4xl font-bold text-center ">No Tasks Yet</h1>
     }
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
             {task.map((tarea, index) => (
                     <TaskCard key={index} tarea={tarea} />
                 ))}
